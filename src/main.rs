4bafd01b50163a72ba9235d67540a062f9669946
David John <davidatgit@gmail.com>
2023-06-14 14:43:24 +0530

    maybe chat

--- src/main.rs
+++ src/main.rs
@@ -13,24 +13,25 @@ async fn listen() -> Result<()> {
     loop {
         let mut buf = [0u8; 1024];
         let (len, source) = socket.recv_from(&mut buf).await?;
-        println!("REQUEST: {source} -> {len} bytes")
+        let msg = String::from_utf8_lossy(&mut buf);
+        println!("{source}: {msg}")
     }
 
     Ok(())
 }
 
-async fn cast() -> Result<()> {
+async fn cast(mut rx: tokio::sync::mpsc::Receiver<String>) -> Result<()> {
     let socket = UdpSocket::bind(SocketAddrV4::new(IFACE, 0)).await?;
     socket
         .connect(SocketAddrV4::new(MULTICAST_ADDR, 5123))
         .await?;
     socket.set_multicast_loop_v4(false)?;
 
-    let data = "hi";
     loop {
-        println!("My IP: {}", socket.local_addr().unwrap().ip());
-        socket.send(data.as_bytes()).await?;
-        std::thread::sleep(Duration::from_secs(10));
+        if let Ok(msg) = rx.try_recv() {
+            socket.send(msg.as_bytes()).await?;
+        }
+        std::thread::sleep(Duration::from_millis(300));
     }
 
     Ok(())
@@ -41,8 +42,18 @@ struct Message {}
 
 #[tokio::main]
 async fn main() {
+    let (tx, rx) = tokio::sync::mpsc::channel(10);
+
     tokio::spawn(async {
-        cast().await.unwrap();
+        cast(rx).await.unwrap();
     });
-    listen().await.unwrap();
+    tokio::spawn(async {
+        listen().await.unwrap();
+    });
+
+    loop {
+        let mut msg = String::new();
+        std::io::stdin().read_line(&mut msg).unwrap();
+        tx.send(msg).await.unwrap();
+    }
 }
