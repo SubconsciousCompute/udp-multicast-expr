4f4588ebd3784887851f823b312e6287f4185374
David John <davidatgit@gmail.com>
2023-06-16 10:58:25 +0530

    some changes

--- src/main.rs
+++ src/main.rs
@@ -13,60 +13,71 @@ async fn listen() -> Result<()> {
     loop {
         let mut buf = [0u8; 2048];
         let (len, source) = socket.recv_from(&mut buf).await?;
-        let packet = bincode::deserialize::<Packet>(&buf[..len]).unwrap();
+        let mut packet = bincode::deserialize::<Discover>(&buf[..len]).unwrap();
+        packet.ip = match source.ip() {
+            IpAddr::V4(addr) => addr,
+            _ => panic!(),
+        };
 
-        // let msg = String::from_utf8_lossy(&mut buf);
-        println!("{}: {}", packet.username, packet.msg);
+        println!("DISCOVERY: {packet:?}")
     }
 
     Ok(())
 }
 
-async fn cast(mut rx: tokio::sync::mpsc::Receiver<String>) -> Result<()> {
+async fn cast(mut rx: tokio::sync::mpsc::Receiver<String>, msg: Discover) -> Result<()> {
     let socket = UdpSocket::bind(SocketAddrV4::new(IFACE, 0)).await?;
     socket
         .connect(SocketAddrV4::new(MULTICAST_ADDR, 5123))
         .await?;
     socket.set_multicast_loop_v4(false)?;
 
-    let username = std::env::args().nth(1).unwrap_or("npc".to_string());
-
     loop {
-        if let Ok(msg) = rx.try_recv() {
-            let bytes = bincode::serialize(&Packet {
-                username: username.to_string(),
-                msg,
-            })
+        socket
+            .send(&bincode::serialize(&msg).unwrap())
+            .await
             .unwrap();
-
-            socket.send(&bytes[..bytes.len()]).await?;
-        }
-        std::thread::sleep(Duration::from_millis(300));
+        std::thread::sleep(Duration::from_secs(30));
     }
 
     Ok(())
 }
 
-#[derive(Serialize, Deserialize)]
-struct Packet {
-    username: String,
-    msg: String,
+#[derive(Serialize, Deserialize, Debug)]
+struct Discover {
+    ip: Ipv4Addr,
+    port: u16,
 }
 
 #[tokio::main]
 async fn main() {
     let (tx, rx) = tokio::sync::mpsc::channel(10);
 
-    tokio::spawn(async {
-        cast(rx).await.unwrap();
+    let socket = UdpSocket::bind("0.0.0.0:0").await.unwrap();
+    let running_on = socket.local_addr().unwrap();
+
+    tokio::spawn(async move {
+        cast(
+            rx,
+            Discover {
+                ip: match running_on.ip() {
+                    IpAddr::V4(addr) => addr,
+                    _ => panic!(),
+                },
+                port: running_on.port(),
+            },
+        )
+        .await
+        .unwrap();
     });
     tokio::spawn(async {
         listen().await.unwrap();
     });
 
     loop {
-        let mut msg = String::new();
-        std::io::stdin().read_line(&mut msg).unwrap();
-        tx.send(msg.trim().to_string()).await.unwrap();
+        let mut buf = [0; 1024];
+        let (data, source) = socket.recv_from(&mut buf).await.unwrap();
+        println!("{data} bytes of data from {}", source.ip());
+        socket.send_to(&buf[..data], source);
     }
 }
